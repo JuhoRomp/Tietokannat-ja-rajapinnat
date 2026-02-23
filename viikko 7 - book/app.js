bae1bf7c30ac714825f0d9e3af43ee5fc6cc9881
Juhoro <juhoromppainen03@gmail.com>
2026-02-23 19:17:44 +0200

    fix book folder from submodule

---
+++ viikko 7 - book/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+
+
+var bookRouter = require('./routes/book');
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/book', bookRouter);
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
